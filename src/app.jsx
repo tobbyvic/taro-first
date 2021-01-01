07fc9267aee004008764d84f045bd5aa7b7386f6
闫玉东-Mac <yanyudong0904@163.com>
2021-01-01 11:56:34 +0800

    taro-cli create finished

---
+++ src/app.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+
+// 假设我们要使用 Redux
+import { Provider } from 'react-redux'
+import configStore from './store'
+
+// 全局样式
+import './app.less'
+
+const store = configStore()
+
+class App extends Component {
+  // 可以使用所有的 React 组件方法
+  componentDidMount () {}
+
+  // 对应 onLaunch
+  onLaunch () {}
+
+  // 对应 onShow
+  componentDidShow () {}
+
+  // 对应 onHide
+  componentDidHide () {}
+
+  render () {
+    // 在入口组件不会渲染任何内容，但我们可以在这里做类似于状态管理的事情
+    return (
+      <Provider store={store}>
+        /* this.props.children 是将要被渲染的页面 */
+        {this.props.children}
+      </Provider>
+    )
+  }
+}
+
+export default App
---
+++ src/pages/index/index.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import { View } from '@tarojs/components'
+
+class Index extends Component {
+  // 可以使用所有的 React 组件方法
+  componentDidMount () {}
+
+  // onLoad
+  onLoad () {}
+
+  // onReady
+  onReady () {}
+
+  // 对应 onShow
+  componentDidShow () {}
+
+  // 对应 onHide
+  componentDidHide () {}
+
+  // 对应 onPullDownRefresh，除了 componentDidShow/componentDidHide 之外，
+  // 所有页面生命周期函数名都与小程序相对应
+  onPullDownRefresh () {}
+
+  render () {
+    return (
+      <View className='index'>
+        {'ddd'}
+      </View>
+    )
+  }
+}
+
+export default Index
